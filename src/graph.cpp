66a143a322707d0ec7f1ee3a66135159276db4a8
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2015-05-01 12:18:42 +0200

    Ca avance doucement

--- src/graph.cpp
+++ src/graph.cpp
@@ -12,7 +12,7 @@ int main (int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		cerr << "Use: ./graph database class" << endl;
+		cerr << "Use: ./graph database class histograms/databse" << endl;
 		exit (1);
 	}
     random_device rd;
@@ -20,6 +20,20 @@ int main (int argc, char* argv[])
     GetFilesInDirectory(fileList, argv[1]);
     vector<string> fileListClass = filtre(fileList, string(argv[1])+"/"+string(argv[2])+"-.*\\.pgm");
     string choosenFile = fileListClass[rd()%fileListClass.size()];
+
+    modifyImage(choosenFile, "tmp.pmg");
+
+    GetFilesInDirectory(fileList, argv[3]);
+
+    imageToHistogram("tmp.pmg");
+	vector<double> histoTmp = readHisto(changeExtension("tmp.pmg"));
+
+    map<string, vector<double>> notations;
+    for(string s : fileList)
+    {
+        vector<double> histo = readHisto(histoFile);
+        notations[extractClass(s)].push_back(EMD(histoTmp,histo));
+    }
 
 
 	return 0;
