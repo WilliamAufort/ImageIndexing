af934b58d8755156d9001a4712645dd9ee105888
William Aufort <william.aufort@ens-lyon.fr>
2015-05-01 12:15:07 +0200

    extractClass, entre autres

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,8 +1,13 @@
 #include <DGtal/base/Common.h>
+#include "../include/toolbox.h"
 
 int main(int argc, char* argv[])
 {
 	DGtal::trace.info() << "Helloworld from DGtal ";
-	DGtal::trace.emphase() << "(version "<< DGTAL_VERSION << ")"<< std::endl;  
+	DGtal::trace.emphase() << "(version "<< DGTAL_VERSION << ")"<< std::endl;
+	std::string filename = "database/tree-1.pgm";
+	DGtal::trace.info() << extractClass(filename) << std::endl;
+	std::string histoname = "histograms/database/device3-10.hist";
+	DGtal::trace.info() << extractClass(histoname) << std::endl;
 	return 0;
 }
